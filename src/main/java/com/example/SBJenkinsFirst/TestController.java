d5411977e4cb7a6f9ddd776063cbb93583136009
Md Amir Hussain <amir4067@gmail.com>
2026-02-26 17:32:39 +0530

    change first git commit

--- src/main/java/com/example/SBJenkinsFirst/TestController.java
+++ src/main/java/com/example/SBJenkinsFirst/TestController.java
@@ -12,7 +12,7 @@ public class TestController {
  @GetMapping("/test")
  public ResponseEntity<Object> getMethodName() {
 	 System.out.println("call test endpoint");
-		System.out.println("main2");
+		System.out.println("main2...");
      return ResponseEntity.ok("success");
  }
 }
